8b477345fd339b3c7070b9dd71acbc83cbd8a70f
Bryan Joseph <bj97301@gmail.com>
2019-02-07 12:33:46 -0800

    format js file and remove extra line brakes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,107 +1,66 @@
 jQuery(function($) {
-
-
-
-
     function seedpay_check_transaction(transaction_id, phone) {
-
         jQuery.post(seedpay_params.ajax_url, {
             'action': 'ajax_seedpay_check_request',
             'transaction_id': transaction_id,
-            'phone': phone
+            'phone': phone,
         }, function(response) {
-            var obj = $.parseJSON(response);
-			
-            if (obj.error == '' && $(".seedpay_payment_cancel").val() == 0) {
-
+            var obj = $.parseJSON(response)
+            if (obj.error == '' && $('.seedpay_payment_cancel').val() == 0) {
                 if (obj.response[0].status == 'acceptedAndPaid') {
-
-                    $(".seedpay_payment_success").val(obj.response[0].status);
-                    $(".seedpay-number-form").hide();
-                    $(".seedpay-number-form-pending").hide();
-                    $(".seedpay-number-form-success").fadeIn();
-                    $(".woocommerce-checkout").submit();
-				}else if (obj.response[0].status == 'rejected') {
-		
-					$(".seedpay_payment_cancel").val('1');
-       				$(".seedpay-number-form-pending").hide();
-        			$(".seedpay-number-form").fadeIn();
-				
-				
-			    } else {
-
+                    $('.seedpay_payment_success').val(obj.response[0].status)
+                    $('.seedpay-number-form').hide()
+                    $('.seedpay-number-form-pending').hide()
+                    $('.seedpay-number-form-success').fadeIn()
+                    $('.woocommerce-checkout').submit()
+                } else if (obj.response[0].status == 'rejected') {
+                    $('.seedpay_payment_cancel').val('1')
+                    $('.seedpay-number-form-pending').hide()
+                    $('.seedpay-number-form').fadeIn()
+                } else {
                     setTimeout(function() {
-
-                        seedpay_check_transaction(transaction_id, phone);
-
-                    }, 5000);
-
-
+                        seedpay_check_transaction(transaction_id, phone)
+                    }, 5000)
                 }
-
             } else {
-					$(".seedpay-messages").html(obj.error);
-					$(".seedpay_payment_cancel").val('1');
-       				$(".seedpay-number-form-pending").hide();
-        			$(".seedpay-number-form").fadeIn();
-                return obj.error;
+                $('.seedpay-messages').html(obj.error)
+                $('.seedpay_payment_cancel').val('1')
+                $('.seedpay-number-form-pending').hide()
+                $('.seedpay-number-form').fadeIn()
+                return obj.error
             }
-
-
-        });
-
+        })
     }
-
-    if ($(".seedpay_recheck_payment").val() == 1) {
-
-        seedpay_check_transaction($(".seedpay_recheck_payment").attr('data-id'), $(".seedpay_recheck_payment").attr('data-pn'));
+    if ($('.seedpay_recheck_payment').val() == 1) {
+        seedpay_check_transaction($('.seedpay_recheck_payment').attr('data-id'), $('.seedpay_recheck_payment').attr('data-pn'))
     }
-
-    $(document).on("click", ".seedpay-cancel-payment-submit", function() {
-
-       // $(".seedpay_payment_cancel").val('1');
-        $(".seedpay-number-form-pending").hide();
-        $(".seedpay-number-form").fadeIn();
-        return false;
-    });
-    $(document).on("click", ".seedpay-request-payment-submit", function() {
-
-		$(".seedpay-messages").empty();
-        $(".seedpay_payment_cancel").val('0')
-        var phone = $("#seedpay_payment_phone").val();
+    $(document).on('click', '.seedpay-cancel-payment-submit', function() {
+        // $(".seedpay_payment_cancel").val('1');
+        $('.seedpay-number-form-pending').hide()
+        $('.seedpay-number-form').fadeIn()
+        return false
+    })
+    $(document).on('click', '.seedpay-request-payment-submit', function() {
+        $('.seedpay-messages').empty()
+        $('.seedpay_payment_cancel').val('0')
+        var phone = $('#seedpay_payment_phone').val()
         jQuery.post(seedpay_params.ajax_url, {
             'action': 'ajax_seedpay_submit_request',
-            'phone': phone
+            'phone': phone,
         }, function(response) {
-
-            var obj = $.parseJSON(response);
-            console.log(obj);
-
+            var obj = $.parseJSON(response)
+            console.log(obj)
             if (obj.error == '') {
-                var transaction_id = obj.request.uniqueTransactionId;
-
-                $(".seedpay_payment_cart_hash").val(obj.request.uniqueTransactionId);
-
-                $(".seedpay-messages").html(obj.response.message);
-                $(".seedpay-number-form").fadeOut();
-                $(".seedpay-number-form-pending").fadeIn();
-
-                seedpay_check_transaction(transaction_id, phone);
-
-
-
-
+                var transaction_id = obj.request.uniqueTransactionId
+                $('.seedpay_payment_cart_hash').val(obj.request.uniqueTransactionId)
+                $('.seedpay-messages').html(obj.response.message)
+                $('.seedpay-number-form').fadeOut()
+                $('.seedpay-number-form-pending').fadeIn()
+                seedpay_check_transaction(transaction_id, phone)
             } else {
-
-
-                $(".seedpay-messages").html(obj.error);
+                $('.seedpay-messages').html(obj.error)
             }
-
-        });
-
-
-
-
-        return false;
-    });
-});
+        })
+        return false
+    })
+})
