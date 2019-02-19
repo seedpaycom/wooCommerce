2ca3eaa67ed47c8d35060b63d5bdaad34ba1167a
Bryan Joseph <bj97301@gmail.com>
2019-02-19 10:17:13 -0800

    wip first test yay!

---
+++ configsTests.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+// require_once __DIR__ . '/configs.php';
+class configsTests extends TestCase
+{
+    public function testMyButts() : void
+    {
+        $this->assertTrue(true);
+    }
+}
