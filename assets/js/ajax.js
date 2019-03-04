04d64e105510bf0427cdb60495f7d1b202958966
Bryan Joseph <bj97301@gmail.com>
2019-03-04 13:55:44 -0800

    wip test all the ajaxes
    use babel env and polyfill

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,1 +1,18 @@
-export default {}
+let submitRequest = function({
+    url,
+    action,
+    parameters,
+    callback,
+    jQuery = require('jquery'),
+}) {
+    jQuery.post(url, {
+        action,
+        parameters,
+    }, function(response) {
+        if (callback) callback(response)
+    })
+}
+
+export default {
+    submitRequest,
+}
--- assets/js/ajaxTests.js
+++ assets/js/ajaxTests.js
@@ -1,5 +1,34 @@
-describe('login', function() {
-    it('true test', function() {
-        true.should.be.false
+import 'babel-polyfill'
+let ajax = require('./ajax').default
+describe('submitRequest', function() {
+    var options
+    beforeEach(() => {
+        options = {
+            url: 'urlz',
+            action: 'actionz',
+            parameters: 'parameterz',
+            jQuery: {
+                post: (url, actionAndParameters, callback) => {
+                    options.postUrl = url
+                    options.postAction = actionAndParameters.action //?
+                    options.postParameters = actionAndParameters.parameters //?
+                    callback()
+                },
+            },
+            callback: (response) => {
+                options.calledCallback = true
+                options.response = response
+            },
+        }
+    })
+    it('jquery posts to the url', function() {
+        let parameters = {
+            phone: 'asdf',
+        }
+        let as = Object.assign({}, parameters) //?
+        options.callback = (response) => {
+            options.postUrl //?
+        }
+        ajax.submitRequest(options) //?
     })
 })
