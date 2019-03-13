6e31048d7b57177d16d661f20c210f4c68c36e81
Bryan Joseph <bj97301@gmail.com>
2019-03-13 15:41:49 -0700

    wip process ajax response

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -15,6 +15,39 @@ let ajax = {
             phoneNumber,
         })
     },
+    generateGenericErrorMessage: (whileDoing) => {
+        return `Error while ${whileDoing}.  Please try again or contact helpdesk@seedpay.com`
+    },
+    processAjaxResponse: ({
+        response,
+        errorHandler,
+        messageHandler,
+        successHandler,
+        genericError,
+    }) => {
+        if (!response) {
+            if (errorHandler) errorHandler(genericError || ajax.generateGenericErrorMessage('sending your request'))
+            return
+        }
+        if (response.error || (response.response && response.response.errors && response.response.errors[0])) {
+            if (errorHandler) errorHandler(response.error || response.response.errors[0])
+            return
+        }
+        if (response.response && response.response.message) {
+            if (messageHandler) messageHandler(response.response.message)
+            return
+        }
+        if (response.response) {
+            if (successHandler) successHandler(response.response)
+            return
+        }
+    },
+    checkUserStatus: async (phoneNumber) => {
+        return await ajax.submitRequest({
+            'action': 'checkUserStatus',
+            phoneNumber,
+        })
+    },
     jQuery: require('jquery'),
 }
 export default ajax
--- assets/js/ajaxTests.js
+++ assets/js/ajaxTests.js
@@ -77,4 +77,92 @@ describe('ajax', () => {
             response.should.equal(options.response)
         })
     })
+    describe('processRequestPaymentResponse', () => {
+        it('calls the error handler with a generic error when wtf is given', async () => {
+            ajax.processAjaxResponse({
+                errorHandler: (errorMessage) => {
+                    options.handlerCalled = true
+                    errorMessage.should.equal(ajax.generateGenericErrorMessage('sending your request'))
+                },
+            })
+
+            options.handlerCalled.should.be.true
+        })
+        it('calls the error handler with given generic error', async () => {
+            let options = {
+                genericError: 'oh wowzer',
+                errorHandler: (errorMessage) => {
+                    options.handlerCalled = true
+                    errorMessage.should.equal(options.genericError)
+                },
+            }
+            ajax.processAjaxResponse(options)
+
+            options.handlerCalled.should.be.true
+        })
+        it('calls the error handler with the response.error', async () => {
+            let response = {
+                error: 'i amz errorzorz',
+            }
+
+            ajax.processAjaxResponse({
+                response,
+                errorHandler: (errorMessage) => {
+                    options.handlerCalled = true
+                    errorMessage.should.equal(response.error)
+                },
+            })
+
+            options.handlerCalled.should.be.true
+        })
+        it('calls the error handler with the response.response.errors[0]', () => {
+            let response = {
+                response: {
+                    errors: ['i amz bettor errorz', ],
+                },
+            }
+
+            ajax.processAjaxResponse({
+                response,
+                errorHandler: (errorMessage) => {
+                    options.handlerCalled = true
+                    errorMessage.should.equal(response.response.errors[0])
+                },
+            })
+
+            options.handlerCalled.should.be.true
+        })
+        it('calls the message handler with the response.response.message', () => {
+            let response = {
+                response: {
+                    message: 'luuk at meezor',
+                },
+            }
+
+            ajax.processAjaxResponse({
+                response,
+                messageHandler: (message) => {
+                    options.handlerCalled = true
+                    message.should.equal(response.response.message)
+                },
+            })
+
+            options.handlerCalled.should.be.true
+        })
+        it('calls the success handler with the response.response', () => {
+            let response = {
+                response: {},
+            }
+
+            ajax.processAjaxResponse({
+                response,
+                successHandler: (successResponse) => {
+                    options.handlerCalled = true
+                    successResponse.should.equal(response.response)
+                },
+            })
+
+            options.handlerCalled.should.be.true
+        })
+    })
 })
